e6e21fa64aec5da6896b5b067660c515d1f331f7
Mateus-de-Moura <mateus.demoura@hotmail.com>
2023-04-12 17:39:46 -0300

    login google

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,12 +1,25 @@
 import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom';
 import axios from 'axios';
 import './Style.css'
-import { Alert } from 'bootstrap';
+import GoogleLogin from 'react-google-login';
+import { gapi } from 'gapi-script';
 
 function Login() {
-  function redirect(){
-     window.location.href = "/Registre";
+
+  const userId = "314159041659-ieqc0r9656ungpfe0osh8458v5ul7h58.apps.googleusercontent.com";
+  const [nomeG, setNome] = useState();
+  const [emailG, setEmail] = useState();
+  const [senhaG, setSenha] = useState();
+  const [fotog, SetFoto] = useState();
+
+
+  gapi.load("client:auth2", () => {
+    gapi.auth2.init({ clientId: userId })
+  })
+
+
+  function redirect() {
+    window.location.href = "/Registre";
   }
 
   localStorage.removeItem("Logado");
@@ -27,15 +40,15 @@ function Login() {
       senha: senha
     })
       .then((response) => {
-
+        window.location.href = "/Home";
         if (response.status === 400) {
           alert("Login ou senha incorretos")
         }
         else {
           var usuario = 'logado';
-          var token = response.data;
+          var token = response.data
           token = token.split(":");
-
+          alert(token);
           localStorage.setItem('IdUser', token[2].trim());
           localStorage.setItem('Logado', usuario);
           localStorage.setItem("Token", token[1].trim());
@@ -48,23 +61,59 @@ function Login() {
       })
 
   }
+  const responseGoogle = (responsee) => {
+    const { profileObj: { name, email, googleId, imageUrl } } = responsee;
+
+    setNome(name);
+    setEmail(email);
+    setSenha(googleId);
+    SetFoto(imageUrl);
+
+    axios.post('https://localhost:44395/User/Register/Google', {
+      nome: nomeG,
+      email: emailG,
+      senha: senhaG,
+      perfil: 1,
+      ativo: 1,
+      base64Photo: fotog
+    })
+      .then((response) => {
+
+        if (response.status === 400) {
+          alert("Login ou senha incorretos")
+        }
+        else {
+          var usuario = 'logado';
+          var token = response.data;
+          token = token.split(":");
+          alert(token[1].trim());
+          localStorage.setItem('IdUser', token[2].trim());
+          localStorage.setItem('Logado', usuario);
+          localStorage.setItem("Token", token[1].trim());
+
+          window.location.href = "/Home";
+        }
+      })
+  }
+
   return (
 
-    
-    <div class="login-form">    
+
+    <div class="login-form">
       <form>
         <div >
           <h2 class="text-center">Login</h2>
-          
+
           <div class="form-group">
             <input id='email' type="text" class="form-control" placeholder="Email" required="required" />
           </div>
           <div class="form-group">
             <input id='senha' type="password" class="form-control" placeholder="Senha" required="required" />
           </div>
+
           <div class="form-group" id='check'>
             <br></br>
-            <input  type="checkbox" id="conect" />
+            <input type="checkbox" id="conect" />
             <label for="conect">Manter-me conectado</label>
           </div>
           <div id='erro'>
@@ -72,12 +121,21 @@ function Login() {
           </div>
           <div class="form-group">
             <button id='enviar' class="btn btn-primary btn-block">Entrar</button>
-            <button onClick={()=> redirect()} id='enviar' class="btn btn-secondary btn-block">Criar conta</button>
+            <button onClick={() => redirect()} id='enviar' class="btn btn-secondary btn-block">Criar conta</button>
+          </div>
+          <div id='loginGoogle'>
+            <GoogleLogin
+              clientId="314159041659-ieqc0r9656ungpfe0osh8458v5ul7h58.apps.googleusercontent.com"
+              buttonText="Continuar com Google"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={'single_host_origin'}
+            />
           </div>
         </div>
       </form>
     </div>
-   
+
   )
 }
 
